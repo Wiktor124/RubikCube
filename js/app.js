72a8523cccd212d34d2e4126b3aed63a1362361d
Víctor Peña Ramírez <victortkd88@gmail.com>
2022-03-01 17:27:33 -0600

    new properties

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-const activecube = document.querySelector(".button");
+// No funciona el cubo por el momento, pero ya va funcionar con lo que voy hacer
+
+const activeCube = document.querySelector(".button");
 const cube = document.querySelector(".cube");
 
-activecube.addEventListener('click', ()=> {
+activeCube.addEventListener('click', ()=> {
     cube.classList.toggle("active-cube");
 });
